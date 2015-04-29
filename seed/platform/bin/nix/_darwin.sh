aec26c0cbb28ce93400a42e144b97810c7b020a9
Ken Stowell <stowell.kt@gmail.com>
2015-04-29 13:55:04 -0700

    Fixing osascript.

--- seed/platform/bin/nix/_darwin.sh
+++ seed/platform/bin/nix/_darwin.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/osascript
 
 osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "mongod" in selected tab of the front window'
 osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "mongo" in selected tab of the front window'
-osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "node "'$1'" --env APP_ENV" in selected tab of the front window'
+osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "node '$1' --env APP_ENV" in selected tab of the front window'
